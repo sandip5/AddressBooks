2be79cad05c87cb09d94b219a9ba5b71607e8606
“sandip5” <singhsandeep.singh97@gmail.com>
2020-06-17 20:44:22 +0530

    ADDED- Ability to delete a person using person name

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -4,8 +4,6 @@ public class AddressBookMain {
     public static void main(String[] args){
         System.out.println("Welcome to Address Book");
         ImplAddressBook util = new ImplAddressBook();
-        //util.addPerson();
-        //util.printPersonDetails();
         util.openAddressBook();
     }
 }
--- IAddressBook.java
+++ IAddressBook.java
@@ -4,5 +4,6 @@ public interface IAddressBook {
     void addPerson();
     void openAddressBook();
     void editPerson();
+    void deletePerson();
 
 }
