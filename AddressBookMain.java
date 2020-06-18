835440f6db437144e7c6445b5638a01686f8702b
“sandip5” <singhsandeep.singh97@gmail.com>
2020-06-18 07:44:04 +0530

    FIXED- Fix Issue for seaching city and state, print person

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,7 +1,7 @@
 package com.bridgelab.addressbook;
 
 public class AddressBookMain {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Welcome to Address Book");
         ImplAddressBook util = new ImplAddressBook();
         util.openAddressBook();
